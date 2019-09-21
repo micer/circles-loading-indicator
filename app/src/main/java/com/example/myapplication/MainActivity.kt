f14cff38965c5f84b2730617a7f09fbf5b9a9ef6
michal <michalcerman@gmail.com>
2019-09-21 17:51:58 +0200

    Code refactoring.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.Completable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -23,79 +19,74 @@ class MainActivity : AppCompatActivity() {
 
         btn_start.setOnClickListener {
 
-//            if (isRunning) {
-//                btn_start.text = "Start"
-//
-//                circle1.animation.repeatCount = 2
-//                circle2.animation.repeatCount = 2
-//                circle3.animation.repeatCount = 2
-//                circle4.animation.repeatCount = 2
-//                circle5.animation.repeatCount = 2
-//                circle6.animation.repeatCount = 2
-//            } else {
-//                btn_start.text = "Stop"
-
-                val delayCoefficient = 5400
-
                 val angleDeg1 = 0.0
-
                 circle1.startAnimation(getBallAnimation(angleDeg1))
 
-                val angleDeg2 = 30.0
-                val subscribe1 = Completable.timer(
-                    delayCoefficient * angleDeg2.toLong(),
-                    TimeUnit.MICROSECONDS,
-                    Schedulers.computation()
+                val circlesList = arrayListOf(
+                    circle2,
+                    circle3,
+                    circle4,
+                    circle5,
+                    circle6
                 )
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        circle2.startAnimation(getBallAnimation(angleDeg2))
-                    }
 
-                val angleDeg3 = 60.0
-                val subscribe2 = Completable.timer(
-                    delayCoefficient * angleDeg3.toLong(),
-                    TimeUnit.MICROSECONDS,
-                    Schedulers.computation()
-                )
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        circle3.startAnimation(getBallAnimation(angleDeg3))
-                    }
+            circlesList.forEachIndexed { index, circle ->
+                val angleDeg = (index + 1) * 30.0
+                Handler().postDelayed({
+                    circle.startAnimation(getBallAnimation(angleDeg))
+                }, DELAY_COEFFICIENT * angleDeg.toLong())
+            }
 
-                val angleDeg4 = 90.0
-                val subscribe3 = Completable.timer(
-                    delayCoefficient * angleDeg4.toLong(),
-                    TimeUnit.MICROSECONDS,
-                    Schedulers.computation()
-                )
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        circle4.startAnimation(getBallAnimation(angleDeg4))
-                    }
 
-                val angleDeg5 = 120.0
-                val subscribe4 = Completable.timer(
-                    delayCoefficient * angleDeg5.toLong(),
-                    TimeUnit.MICROSECONDS,
-                    Schedulers.computation()
-                )
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        circle5.startAnimation(getBallAnimation(angleDeg5))
-                    }
-
-                val angleDeg6 = 150.0
-                val subscribe5 = Completable.timer(
-                    delayCoefficient * angleDeg6.toLong(),
-                    TimeUnit.MICROSECONDS,
-                    Schedulers.computation()
-                )
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        circle6.startAnimation(getBallAnimation(angleDeg6))
-                    }
-
+//                val angleDeg2 = 30.0
+//                Handler().postDelayed({
+//                    circle2.startAnimation(getBallAnimation(angleDeg2))
+//                }, delayCoefficient * angleDeg2.toLong())
+//
+//                val angleDeg3 = 60.0
+//                val subscribe2 = Completable.timer(
+//                    delayCoefficient * angleDeg3.toLong(),
+//                    TimeUnit.MICROSECONDS,
+//                    Schedulers.computation()
+//                )
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe {
+//                        circle3.startAnimation(getBallAnimation(angleDeg3))
+//                    }
+//
+//                val angleDeg4 = 90.0
+//                val subscribe3 = Completable.timer(
+//                    delayCoefficient * angleDeg4.toLong(),
+//                    TimeUnit.MICROSECONDS,
+//                    Schedulers.computation()
+//                )
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe {
+//                        circle4.startAnimation(getBallAnimation(angleDeg4))
+//                    }
+//
+//                val angleDeg5 = 120.0
+//                val subscribe4 = Completable.timer(
+//                    delayCoefficient * angleDeg5.toLong(),
+//                    TimeUnit.MICROSECONDS,
+//                    Schedulers.computation()
+//                )
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe {
+//                        circle5.startAnimation(getBallAnimation(angleDeg5))
+//                    }
+//
+//                val angleDeg6 = 150.0
+//                val subscribe5 = Completable.timer(
+//                    delayCoefficient * angleDeg6.toLong(),
+//                    TimeUnit.MICROSECONDS,
+//                    Schedulers.computation()
+//                )
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe {
+//                        circle6.startAnimation(getBallAnimation(angleDeg6))
+//                    }
+//==================================
 //            val angleDeg7 = 120.0
 //            Completable.timer(delayCoefficient * angleDeg7.toLong(), TimeUnit.MICROSECONDS, Schedulers.computation())
 //                .observeOn(AndroidSchedulers.mainThread())
@@ -133,11 +124,17 @@ class MainActivity : AppCompatActivity() {
 
         val animation = TranslateAnimation(0f, xDelta, 0f, yDelta)
         animation.interpolator = AccelerateDecelerateInterpolator()
-        animation.duration = 1000
-        animation.repeatCount = 5
+        animation.duration = ANIMATION_DURATION
+        animation.repeatCount = 100
         animation.repeatMode = TranslateAnimation.REVERSE
 //        animation.startOffset = (3 * angleDeg).toLong()
 
         return animation
+    }
+
+    companion object {
+        // ~ 54 delay duration when animation duration 10000 ms
+        private const val DELAY_COEFFICIENT = 54
+        private const val ANIMATION_DURATION : Long = 10_000
     }
 }
