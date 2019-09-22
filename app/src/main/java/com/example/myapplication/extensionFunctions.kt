78022b6a15284b1fd815745ab6d17df5116c2a76
michal <michalcerman@gmail.com>
2019-09-22 16:47:11 +0200

    Created circles loading indicator module.

--- app/src/main/java/com/example/myapplication/extensionFunctions.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-import android.content.res.Resources
-
-fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
-
-fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
---
+++ circlesloadingindicator/src/main/java/eu/micer/circlesloadingindicator/CirclesLoadingIndicator.kt
@@ -0,0 +1,113 @@
+package eu.micer.circlesloadingindicator
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
+import kotlin.math.cos
+import kotlin.math.sin
+
+class CirclesLoadingIndicator @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private val circlesColor: Int
+    private val circlesCount: Int
+    private val circlesRadius: Float
+    private val rotationDuration: Int
+    private val showLeadingCircle: Boolean
+
+    private lateinit var firstPaint: Paint
+    private lateinit var secondPaint: Paint
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CirclesLoadingIndicator,
+            0, 0
+        ).apply {
+
+            try {
+                circlesCount = getInt(R.styleable.CirclesLoadingIndicator_circlesCount, 6)
+                circlesRadius = getDimension(R.styleable.CirclesLoadingIndicator_circlesRadius, 16f)
+                circlesColor =
+                    getColor(R.styleable.CirclesLoadingIndicator_circlesColor, Color.BLACK)
+                rotationDuration =
+                    getInt(R.styleable.CirclesLoadingIndicator_rotationDuration, 3000)
+                showLeadingCircle =
+                    getBoolean(R.styleable.CirclesLoadingIndicator_showLeadingCircle, false)
+            } finally {
+                recycle()
+            }
+        }
+
+        startAnimation()
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        firstPaint = Paint()
+        firstPaint.apply {
+            strokeWidth = 2f
+            style = Paint.Style.STROKE
+        }
+
+        secondPaint = Paint()
+        secondPaint.apply {
+            color = circlesColor
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        canvas?.let {
+            val startAngle = -Math.PI / 2f
+            val centerX = width / 2
+            val centerY = height / 2
+
+            if (showLeadingCircle) {
+                canvas.drawCircle(
+                    centerX.toFloat(),
+                    centerY.toFloat(),
+                    width / 2f - circlesRadius,
+                    firstPaint
+                )
+            }
+
+            for (i in 0 until circlesCount) {
+                val angle = if (i == 0) startAngle
+                else startAngle + (i * ((2 * Math.PI) / circlesCount))
+
+                val x = centerX + cos(angle) * (width / 2f - circlesRadius)
+                val y = centerY + sin(angle) * (width / 2f - circlesRadius)
+
+                canvas.drawCircle(x.toFloat(), y.toFloat(), circlesRadius, secondPaint)
+            }
+        }
+    }
+
+    private fun startAnimation() {
+        val rotateAnimation = RotateAnimation(
+            0f,
+            359f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        ).apply {
+            duration = rotationDuration.toLong()
+            interpolator = LinearInterpolator()
+            repeatCount = Animation.INFINITE
+        }
+        startAnimation(rotateAnimation)
+    }
+}
